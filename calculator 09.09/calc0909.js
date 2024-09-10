44df17a82b1fbb546f4cf0de6a5e671ab70d08c2
MariaFilippock <Kasyan96@yandex.ru>
2024-09-10 13:16:59 +0200

    change func chooseNumber

--- calculator 09.09/calc0909.js
+++ calculator 09.09/calc0909.js
@@ -2,11 +2,24 @@ const inputCalculator = document.querySelector('.input_calculator');
 const equalBtn = document.querySelector('.equal');
 const Btns = document.querySelector('.calculator_buttons');
 
+
 Btns.addEventListener('click', chooseNumber);
+
+
+let data = {
+    numbers: [],
+    operator: '+' // '-', '/', '*', '%'
+}
 
 function chooseNumber(event) {
     event.preventDefault();
+
     const chosenSign = event.target.closest('.button');
-    inputCalculator.value = chosenSign.innerText;
+
+    inputCalculator.value += chosenSign.innerText;
+
+    let input = inputCalculator.value;
+
+    data.numbers.push(input);
 }
 
