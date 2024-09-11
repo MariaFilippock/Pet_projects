906c8ea1181e1907370289f45606703001277ace
MariaFilippock <Kasyan96@yandex.ru>
2024-09-11 09:50:56 +0200

    update calculator

--- calculator 09.09/calc0909.js
+++ calculator 09.09/calc0909.js
@@ -1,25 +1,69 @@
 const inputCalculator = document.querySelector('.input_calculator');
-const equalBtn = document.querySelector('.equal');
-const Btns = document.querySelector('.calculator_buttons');
+const btnNumber = document.querySelectorAll('.number');
+const btnAction = document.querySelectorAll('.button_action');
+const btnEqual = document.querySelector('.equal');
+const btnReset = document.querySelector('.button_reset');
 
 
-Btns.addEventListener('click', chooseNumber);
+btnNumber.forEach((el) => el.addEventListener('click', handleNumberClick));
+btnAction.forEach((el) => el.addEventListener('click', handleSignClick));
+btnEqual.addEventListener('click', handleResultClick);
+btnReset.addEventListener('click', handleResetClick);
 
-
-let data = {
-    numbers: [],
-    operator: '+' // '-', '/', '*', '%'
+let state = {
+    firstArg: '',
+    secondArg: '',
+    operator: '' // '-', '/', '*', '%', '+'
 }
 
-function chooseNumber(event) {
-    event.preventDefault();
+//ф-ция, которая должна что-то делать если нажали на ЧИСЛО
+function handleNumberClick(event) {
+    if (state.operator) {
+        state.secondArg += event.target.innerText;
+    } else {
+        state.firstArg += event.target.innerText;
+    }
+    renderDisplay();
+}
 
-    const chosenSign = event.target.closest('.button');
+//ф-ция, которая должна что-то делать если нажали на ЗНАК ДЕЙСТВИЯ
+function handleSignClick(event) {
+    state.operator = event.target.innerText;
 
-    inputCalculator.value += chosenSign.innerText;
+    renderDisplay();
+}
 
-    let input = inputCalculator.value;
+function renderDisplay() {
+    inputCalculator.value = `${state.firstArg} ${state.operator} ${state.secondArg}`;
 
-    data.numbers.push(input);
+}
+
+
+function handleResultClick() {
+    let result = 0;
+    if (state.operator === '-') {
+        result = Number(state.firstArg) - Number(state.secondArg);
+    } else if (state.operator === '+') {
+        result = Number(state.firstArg) + Number(state.secondArg);
+    } else if (state.operator === '×') {
+        result = Number(state.firstArg) * Number(state.secondArg);
+    } else if (state.operator === '÷') {
+        result = Number(state.firstArg) / Number(state.secondArg);
+    }
+
+    state.firstArg = result;
+    state.secondArg = '';
+    state.operator = '';
+
+    renderDisplay();
+}
+
+
+function handleResetClick() {
+    state.firstArg = '';
+    state.operator = '';
+    state.secondArg = '';
+
+    renderDisplay();
 }
 
